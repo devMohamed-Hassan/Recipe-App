875ba26fef68e56eaf942ce4461a00c34ecafd12
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-06-25 07:40:55 +0300

    feat: add Area page

--- src/Components/Area/Area.jsx
+++ src/Components/Area/Area.jsx
@@ -3,10 +3,23 @@ import styles from './Area.module.css'
 
 function Area() {
   return (
-     <>
-     <div>Area</div>
-     </>
-    
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center p-8">
+        <div className="bg-white rounded-2xl shadow-lg p-8 lg:p-12 max-w-md mx-auto">
+          <div className="text-6xl lg:text-8xl mb-6 text-[#F29724]">
+           <i class="fa-solid fa-earth-americas"></i>
+          </div>
+          <h1 className="text-2xl lg:text-3xl font-cursive font-bold mb-4 bg-gradient-to-r from-[#F29624] to-[#DE6241] bg-clip-text text-transparent">
+            Area Component
+          </h1>
+          <div className="bg-gray-50  rounded-lg p-4">
+            <p className="text-lg text-gray-500 font-cursive">
+             مكسل اعمله
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
