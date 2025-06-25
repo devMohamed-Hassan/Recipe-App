309667a32f60b949f8935beebec32297ffe44547
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-06-25 07:40:25 +0300

    feat: add Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,10 +3,17 @@ import styles from './Footer.module.css'
 
 function Footer() {
   return (
-     <>
-     <div>Footer</div>
-     </>
-    
+    <footer className="bg-white border-t border-gray-200 py-6 lg:py-8">
+      <div className="container mx-auto px-4 lg:px-6">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-4">
+          <div className="text-center lg:text-left ">
+            <h3 className="text-lg lg:text-2xl font-semibold  mb-2 bg-gradient-to-r from-[#F29624] to-[#DE6241] bg-clip-text text-transparent font-cursive">
+              Footer
+            </h3>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
